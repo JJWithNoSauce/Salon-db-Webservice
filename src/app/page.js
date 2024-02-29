c3403e2dae41ca277c0e7823a3437078fa88d9e8
yeaw02386 <yeaw02386@gmail.com>
2024-02-29 19:17:22 +0700

    reconnect database

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,13 @@
 import Image from "next/image";
-import { db,ssh } from "./db";
+import { db} from "./db";
 
 export default function Homepage() {
-
-  db.then(client => {
-    client.query('select * from Persons', function (err, results, fields) {
-        console.log(results);
-    });
-  })
-
-  db.then(client => {
-    client.query('select * from Persons', function (err, results, fields) {
-        console.log(results);
-        
-    });
-  })
-
   
+  db().then(client => {
+    client.query('select * from Persons', function (err, results, fields) {
+        console.log(results);
+    });
+  })
 
   return (
     <main className="Homepage">
