4ea7fc437f3ed2c6b52ca30012d39204d54bb820
JJWithNoSauce <KimisanaKentai@protonmail.com>
2024-03-04 23:40:45 +0700

    commission wip

---
+++ src/app/api/getcomcal/route.js
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+import { db,ssh } from "@/app/db";
+
+export async function GET(req){
+    const client = await db(); 
+    const results = await new Promise((resolve, reject) => {
+      client.query('SELECT * FROM ServiceBillingDate',(error, results, fields) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+        resolve(results);
+      });
+    });
+    ssh.close();
+    return NextResponse.json(results);
+}
