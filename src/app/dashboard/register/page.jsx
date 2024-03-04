1a80527972d868e60ea2bca96661e8438c41f868
Peerasanti Sriputta <peerasanti.s@kkumail.com>
2024-03-04 19:17:52 +0700

    add insert product function in app file

--- src/app/dashboard/register/page.jsx
+++ src/app/dashboard/register/page.jsx
@@ -1,4 +1,4 @@
-import { register } from "./action"
+import { addCustomer } from "@/app/app"
 import Link from "next/link"
 
 export default function detail(){
@@ -6,15 +6,15 @@ export default function detail(){
     return(
         <> 
             <div>
-            <form action={register()}>
+            <form action={addCustomer}>
                 <input name="phone" type="text" placeholder="กรอกเบอร์โทร" className="input input-bordered w-full max-w-xs"/>
+                <br />
                 <input name="name" type="text" placeholder="กรอกชื่อ-นามสกุล" className="input input-bordered w-full max-w-xs"/>
+                <br />
+                <button className="btn" type="submit" href={"/dashboard/register/success"}>
+                    ตกลง
+                </button>
             </form>
-            </div>
-            <div>
-                <Link href="/dashboard/register/success">
-                    <button className="btn">ตกลง</button>
-                </Link>
             </div>
         </>
     )
