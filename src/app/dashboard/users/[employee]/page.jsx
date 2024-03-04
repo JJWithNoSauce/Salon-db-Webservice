f7002059b2ca26d43126c7e6216bdf5cb45ff6ed
Peerasanti Sriputta <peerasanti.s@kkumail.com>
2024-03-05 04:13:22 +0700

    edit user page

--- src/app/dashboard/users/[employee]/page.jsx
+++ src/app/dashboard/users/[employee]/page.jsx
@@ -1,13 +1,30 @@
 import React from "react"
 import style from "../user.module.css"
 import { editProduct } from "@/app/app"
+import { db, ssh } from "@/app/db"
 
-const productMaintain = async ({params}) => {
+const productMaintain = async ({ params }) => {
+
+    const id_query = Number(params.employee)
 
     const res= await fetch('http://localhost:3000/api/getemployee/'+Number(params.employee),{
         next: {revalidate:1}
     })
     const employee = await res.json()
+
+    const client = await db(); 
+    const skill = await new Promise((resolve, reject) => {
+      client.query('SELECT service_name from Employee, EmployeeSkill, Service WHERE Employee.em_id = EmployeeSkill.em_id AND EmployeeSkill.service_id = Service.service_id AND Employee.em_id = '+id_query, (error, results, fields) => {
+        if (error) {
+          reject(error);
+          return;
+            }
+            resolve(results);
+        });
+        });
+        ssh.close();
+
+    console.log(skill)
 
     return (
         <div>
@@ -19,9 +36,14 @@ const productMaintain = async ({params}) => {
                 <div class="text-xl">{"Phone: " + employee[0].em_email}</div>
             
             <div class="divider text-xl"></div>
-            {/* <div>
-                <h2 class="text-xl">ความสามารถ</h2>
-            </div> */}
+            <div class="divider text-xl">ความสามารถ</div>
+            <div>
+                {skill.map(item=>(
+                    <div key={item.service_name}>
+                            <h2>{item.service_name}</h2>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
