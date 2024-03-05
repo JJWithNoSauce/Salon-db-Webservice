e2cb966d7204492976a732edd4a677927162a349
yeaw02386 <yeaw02386@gmail.com>
2024-03-05 08:28:37 +0700

    add api

--- src/app/dashboard/services/[service_id]/page.jsx
+++ src/app/dashboard/services/[service_id]/page.jsx
@@ -14,17 +14,12 @@ export default async function employee({ params }){
         service_id:params.service_id
       })
       });
-      
+
     const resultEmployee = await res.json();
-    console.log(resultEmployee)
+
 
     function setdata(data){
       let arr
-      if(typeof window !== 'undefined'){
-          arr = localStorage.getItem("ser")
-          arr[arr.length-1].em = data
-          localStorage.setItem("ser",arr)
-      }
     }
 
     return(
@@ -32,12 +27,13 @@ export default async function employee({ params }){
             <div className={styles.container}>
                 {resultEmployee.map(item=>(
                     <div key={item.em_id}>
-                        <Link href={'/dashboard/services/detail'} onClick={setdata(item.em_id)}>
+                        <Link href={{ pathname:'/dashboard/services/detail',query:
+                          {
+                            em_id: item.em_id,
+                            ser_id : params.service_id
+                          }
+                      }}>
                             <h2 className={styles.service}>ช่าง {item.em_name}</h2>
-                            <br />
-                            <h4 className={styles.service}>ติดต่อ : {item.em_phone}</h4>
-                            <br />
-                            <h4 className={styles.service}>{item.em_email}</h4>
                         </Link>
                     </div>
                 ))}
