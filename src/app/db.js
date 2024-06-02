07efdcb012370d84fc0f8a35f20ab3a207022708
JJWithNoSauce <93840770+JJWithNoSauce@users.noreply.github.com>
2024-06-02 17:21:27 +0700

    Update db.js

--- src/app/db.js
+++ src/app/db.js
@@ -3,9 +3,9 @@ export const ssh = require('mysql-ssh');
 export function db(){
     const connect = ssh.connect(
         {
-            host: '10.147.18.158',
-            user: 'mmn',
-            password: '@Zx12345'
+            host: '###.###.###.###',
+            user: '#########',
+            password: '#########'
         },
         {
             host: 'localhost',
